948e81accc3e4e796225e3be7ee6f9c97bfe1925
arondy <antongatcenko@gmail.com>
2026-08-03 19:47:57 +0300

    feat(app): реализована сборка приложения из слоёв репозиториев, сервисов и обработчиков

---
+++ ota-orchestrator/internal/core/app.go
@@ -0,0 +1,44 @@
+package core
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Arondy/OTA-Firmware-Orchestrator/internal/core/config"
+	"github.com/Arondy/OTA-Firmware-Orchestrator/internal/core/repository/postgres"
+	"github.com/Arondy/OTA-Firmware-Orchestrator/internal/core/service"
+	core_http "github.com/Arondy/OTA-Firmware-Orchestrator/internal/core/transport/http"
+	"github.com/Arondy/OTA-Firmware-Orchestrator/internal/core/transport/http/handlers"
+	"github.com/Arondy/OTA-Firmware-Orchestrator/internal/core/transport/http/middleware"
+	"go.uber.org/zap"
+)
+
+func Run(ctx context.Context, config *config.Config, logger *zap.SugaredLogger) error {
+	db, err := postgres.NewDB(ctx, config.DB)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	deviceRepo := postgres.NewDeviceRepo(db)
+	firmwareVersionRepo := postgres.NewFirmwareVersionRepo(db)
+	rolloutCampaignRepo := postgres.NewRolloutCampaignRepo(db)
+
+	deviceSvc := service.NewDeviceService(deviceRepo)
+	firmwareVersionSvc := service.NewFirmwareVersionService(firmwareVersionRepo)
+	rolloutCampaignSvc := service.NewRolloutCampaignService(rolloutCampaignRepo, firmwareVersionRepo)
+
+	healthAPI := handlers.NewHealthHandler()
+	deviceAPI := handlers.NewDeviceHandler(deviceSvc)
+	firmwareVersionAPI := handlers.NewFirmwareVersionHandler(firmwareVersionSvc)
+	rolloutCampaignAPI := handlers.NewRolloutCampaignHandler(rolloutCampaignSvc)
+
+	var router http.Handler = core_http.NewRouter(healthAPI, deviceAPI, firmwareVersionAPI, rolloutCampaignAPI)
+	router = middleware.WrapInMiddleware(router, logger)
+	server := core_http.NewServer(router, &config.HTTPServer, logger.Named("Server"))
+
+	if err := server.Run(ctx); err != nil {
+		return err
+	}
+	return nil
+}
