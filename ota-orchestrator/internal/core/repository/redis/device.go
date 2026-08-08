c7a830ba2a55b668c0aeab04eb5365b5238860dc
arondy <antongatcenko@gmail.com>
2026-08-09 00:16:36 +0300

    feat(redis): добавлены репозитории кэша для кампаний и устройств на Redis

---
+++ ota-orchestrator/internal/core/repository/redis/device.go
@@ -0,0 +1,47 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Arondy/OTA-Firmware-Orchestrator/internal/core/config"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+)
+
+type DeviceCacheRepo struct {
+	*redis.Client
+	key               string
+	lastSeenTTL       time.Duration
+	currentVersionTTL time.Duration
+}
+
+func NewDeviceCacheRepo(rdb *redis.Client, config config.CacheConfig) *DeviceCacheRepo {
+	return &DeviceCacheRepo{
+		Client:            rdb,
+		key:               "device",
+		lastSeenTTL:       config.CacheDeviceLastSeenTTL,
+		currentVersionTTL: config.CacheDeviceCurrentVersionTTL,
+	}
+}
+
+func (r *DeviceCacheRepo) GetCurrentVersion(ctx context.Context, id uuid.UUID) (string, error) {
+	key := fmt.Sprintf("%s:%s:current_version", r.key, id)
+	return r.Get(ctx, key).Result()
+}
+
+func (r *DeviceCacheRepo) SetCurrentVersion(ctx context.Context, id uuid.UUID, currentVersion string) error {
+	key := fmt.Sprintf("%s:%s:current_version", r.key, id)
+	return r.Set(ctx, key, currentVersion, r.currentVersionTTL).Err()
+}
+
+func (r *DeviceCacheRepo) GetLastSeen(ctx context.Context, id uuid.UUID) (time.Time, error) {
+	key := fmt.Sprintf("%s:%s:last_seen", r.key, id)
+	return r.Get(ctx, key).Time()
+}
+
+func (r *DeviceCacheRepo) SetLastSeen(ctx context.Context, id uuid.UUID, lastSeen time.Time) error {
+	key := fmt.Sprintf("%s:%s:last_seen", r.key, id)
+	return r.Set(ctx, key, lastSeen, r.lastSeenTTL).Err()
+}
---
+++ ota-orchestrator/internal/core/repository/redis/redis.go
@@ -0,0 +1,25 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Arondy/OTA-Firmware-Orchestrator/internal/core/config"
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient(ctx context.Context, config config.CacheConfig) (*redis.Client, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:         fmt.Sprintf("%s:%d", config.Host, config.Port),
+		ReadTimeout:  500 * time.Millisecond,
+		WriteTimeout: 500 * time.Millisecond,
+	})
+
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		_ = rdb.Close()
+		return nil, fmt.Errorf("failed redis ping: %w", err)
+	}
+
+	return rdb, nil
+}
