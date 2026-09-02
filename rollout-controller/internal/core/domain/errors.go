9ac68083d29329b18da91220ff6d616d204ecad9
arondy <antongatcenko@gmail.com>
2026-09-02 21:37:57 +0300

    feat(domain): добавлен ErrNotEnoughSamples при недостаточной выборке

--- rollout-controller/internal/core/domain/errors.go
+++ rollout-controller/internal/core/domain/errors.go
@@ -4,3 +4,4 @@ import "errors"
 
 var ErrUpdateResultNotProduced = errors.New("failed to produce update result")
 var ErrCurrentStageNotFound = errors.New("current stage for this campaign wasn't found")
+var ErrNotEnoughSamples = errors.New("current stage hasn't reached minimum sample size for evaluation")
