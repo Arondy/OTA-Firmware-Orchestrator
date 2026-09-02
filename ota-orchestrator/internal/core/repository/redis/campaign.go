11815f1348cd5acc1c306b8a5f403ffc6a84a13b
arondy <antongatcenko@gmail.com>
2026-09-02 18:22:44 +0300

    feat(redis): добавлен сет активных кампаний

--- ota-orchestrator/internal/core/repository/redis/campaign.go
+++ ota-orchestrator/internal/core/repository/redis/campaign.go
@@ -54,3 +54,29 @@ func (r *CampaignCacheRepo) DeleteCurrentTargetPercent(ctx context.Context, id u
 	key := fmt.Sprintf("%s:%s:current_target_percent", r.key, id)
 	return r.client.Del(ctx, key).Err()
 }
+
+func (r *CampaignCacheRepo) AddRunningCampaigns(ctx context.Context, ids ...uuid.UUID) error {
+	key := fmt.Sprintf("%s:running_campaigns", r.key)
+	anySlice := uuidSliceToAny(ids)
+	return r.client.SAdd(ctx, key, anySlice...).Err()
+}
+
+func (r *CampaignCacheRepo) RemoveRunningCampaigns(ctx context.Context, ids ...uuid.UUID) error {
+	key := fmt.Sprintf("%s:running_campaigns", r.key)
+	anySlice := uuidSliceToAny(ids)
+	return r.client.SRem(ctx, key, anySlice...).Err()
+}
+
+func (r *CampaignCacheRepo) DeleteAllRunningCampaigns(ctx context.Context) error {
+	key := fmt.Sprintf("%s:running_campaigns", r.key)
+	return r.client.Del(ctx, key).Err()
+}
+
+func uuidSliceToAny(ids []uuid.UUID) []any {
+	anySlice := make([]any, len(ids))
+	for i, id := range ids {
+		anySlice[i] = id
+	}
+
+	return anySlice
+}
