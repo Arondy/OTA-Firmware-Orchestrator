145dfc4e9f654d26f796ae46f1bb3d658fbdba42
arondy <antongatcenko@gmail.com>
2026-09-07 18:59:10 +0300

    feat(controller/config): добавлены параметры таймаутов для кэша и брокера сообщений

--- rollout-controller/internal/core/config/config.go
+++ rollout-controller/internal/core/config/config.go
@@ -29,6 +29,8 @@ type ServerConfig struct {
 type CacheConfig struct {
 	Host                   string        `koanf:"CACHE_HOST" validate:"required"`
 	Port                   int           `koanf:"CACHE_PORT" validate:"required"`
+	ReadTimeout            time.Duration `koanf:"CACHE_READ_TIMEOUT" validate:"required"`
+	WriteTimeout           time.Duration `koanf:"CACHE_WRITE_TIMEOUT" validate:"required"`
 	CampaignEventIDSeenTTL time.Duration `koanf:"CACHE_CAMPAIGN_EVENT_ID_SEEN_TTL" validate:"required"`
 }
 
@@ -38,13 +40,17 @@ const (
 )
 
 type BrokerConfig struct {
-	Host         string        `koanf:"BROKER_HOST" validate:"required"`
-	Port         int           `koanf:"BROKER_PORT" validate:"required"`
-	BatchTimeout time.Duration `koanf:"BROKER_BATCH_TIMEOUT" validate:"required"`
-	Timeout      time.Duration `koanf:"BROKER_TIMEOUT" validate:"required"`
-	GroupID      string        `koanf:"BROKER_GROUP_ID" validate:"required"`
-	MinBytes     int           `koanf:"BROKER_MIN_BYTES" validate:"required,min=1"`
-	Topic        string
+	Host           string        `koanf:"BROKER_HOST" validate:"required"`
+	Port           int           `koanf:"BROKER_PORT" validate:"required"`
+	BatchTimeout   time.Duration `koanf:"BROKER_BATCH_TIMEOUT" validate:"required"`
+	Timeout        time.Duration `koanf:"BROKER_TIMEOUT" validate:"required"`
+	GroupID        string        `koanf:"BROKER_GROUP_ID" validate:"required"`
+	MinBytes       int           `koanf:"BROKER_MIN_BYTES" validate:"required,min=1"`
+	ReaderMaxWait  time.Duration `koanf:"BROKER_READER_MAX_WAIT" validate:"required"`
+	CommitTimeout  time.Duration `koanf:"BROKER_COMMIT_TIMEOUT" validate:"required"`
+	DLQTimeout     time.Duration `koanf:"BROKER_DLQ_TIMEOUT" validate:"required"`
+	DLQMaxAttempts int           `koanf:"BROKER_DLQ_MAX_ATTEMPTS" validate:"required,min=1"`
+	Topic          string
 }
 
 type EvaluatorConfig struct {
